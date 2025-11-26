d5f47e2a6cf059fd43723f79390da3d676284ec9
Noah K (2ofeveryanimal everywhere) <41722860+2of@users.noreply.github.com>
2025-11-26 20:48:47 +1300

    Initial Commit

---
+++ QuickMonitorSettings/QuickMonitorSettingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickMonitorSettingsApp.swift
+//  QuickMonitorSettings
+//
+//  Created by Noah King on 26/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct QuickMonitorSettingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
